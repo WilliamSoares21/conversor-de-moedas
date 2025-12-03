e6a5faf908fdc03cc30cb1b7aec51581403343f6
William Soares <pedro.williamsoares2022@gmail.com>
2025-12-03 14:34:13 -0300

    refact: Estrutura para iniciar a refatoração

---
+++ conversor-de-moedas/src/main/java/com/conversor/conversor_de_moedas/ConversorDeMoedasApplication.java
@@ -0,0 +1,13 @@
+package com.conversor.conversor_de_moedas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversorDeMoedasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversorDeMoedasApplication.class, args);
+	}
+
+}
---
+++ conversor-de-moedas/src/test/java/com/conversor/conversor_de_moedas/ConversorDeMoedasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conversor.conversor_de_moedas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversorDeMoedasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
